ea7d6a8180da23c79ac235584400edf46f7b0b1a
bowtoyourlord <119819966+bowtoyourlord@users.noreply.github.com>
2022-12-06 20:44:32 +0200

    Update BigInt.cpp

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -6,7 +6,7 @@ Big_Int::Big_Int(size_t SIZE, unsigned long long fill_with)
 {
 	this->SIZE = SIZE;
 	number = new unsigned long long[this->SIZE];
-	for (int i = 0; i < this->SIZE; ++i)
+	for (size_t i = 0; i < this->SIZE; ++i)
 		number[i] = fill_with;
 }
 
@@ -30,7 +30,7 @@ Big_Int::Big_Int(string str)
 			if(str.size() % insides.BASE_POWER != 0) SIZE++;
 			number = new unsigned long long[SIZE];
 			string substring;
-			for (int g = 0; g < SIZE-1; ++g)
+			for (size_t g = 0; g < SIZE-1; ++g)
 			{
 				position -= insides.BASE_POWER;
 				substring = str.substr(position, insides.BASE_POWER);
@@ -50,18 +50,18 @@ Big_Int::Big_Int(unsigned long long other)
 	}
 	else
 	{
-		int digits = log10(other) + 1;
+		size_t digits = log10(other) + 1;
 		while (digits > insides.BASE_POWER)
 		{
 			digits -= insides.BASE_POWER;
 			SIZE++;
 		}
 		number = new unsigned long long[SIZE];
-		int i = 0;
+		size_t i = 0;
 		for (i; i < SIZE - 1; ++i)
 		{
-			number[i] = other % int(pow(insides.MODULO, (i + 1))); //cmath pow returns double ; explicit int cast required
-			other /= int(pow(insides.MODULO, (i + 1)));
+			number[i] = other % size_t(pow(insides.MODULO, (i + 1))); //cmath pow returns double ; explicit int cast required
+			other /= size_t(pow(insides.MODULO, (i + 1)));
 		}
 		number[i] = other;
 	};
@@ -120,13 +120,13 @@ Big_Int Big_Int::operator+(const Big_Int& other) const
 	Big_Int result(max(this->SIZE, other.SIZE) + 1, 0);
 	if (this -> negative && other.negative) result.negative=true;
 
-	int carry = 0;
+	size_t carry = 0;
 
-	int i = 0;
+	size_t i = 0;
 
 	for (i; i < min(this->SIZE, other.SIZE); ++i)
 	{
-		int tmp = this->number[i] + other.number[i] + carry;
+		size_t tmp = this->number[i] + other.number[i] + carry;
 		carry = tmp / insides.MODULO;
 		tmp %= insides.MODULO;
 		result.number[i] = tmp;
@@ -175,9 +175,9 @@ Big_Int Big_Int::operator-(const Big_Int& other) const
 	}
 	Big_Int result((this->SIZE) +1, 0);
 
-	int carry = 0;
+	size_t carry = 0;
 
-	int i = 0;
+	size_t i = 0;
 
 	for (i; i < other.SIZE; ++i)
 	{
@@ -225,10 +225,10 @@ Big_Int Big_Int::naive_multiplication(const Big_Int& first,const Big_Int& other)
 	Big_Int result(first.SIZE + other.SIZE, 0);
 	if (first.negative ^ other.negative) result.negative = true;
 	{
-		for (int i = 0; i < other.SIZE; ++i)
+		for (size_t i = 0; i < other.SIZE; ++i)
 		{
-			int carry = 0;
-			for (int j = 0; j < first.SIZE; ++j)
+			size_t carry = 0;
+			for (size_t j = 0; j < first.SIZE; ++j)
 			{
 				result.number[i + j] += carry + first.number[j] * other.number[i];
 				carry = result.number[i + j] / insides.MODULO;
@@ -245,24 +245,24 @@ Big_Int Big_Int::Karatsuba_multiplication(Big_Int first, Big_Int second)
 	Big_Int result(first.SIZE + second.SIZE, 0);
 	if (first.negative ^ second.negative) result.negative = true;
 	_to_equal_size(first, second);
-	int k = first.SIZE / 2;
+	size_t k = first.SIZE / 2;
 	Big_Int x = _pow(10, insides.BASE_POWER*k);
 	Big_Int x2 = _pow(x,2);
 
 	Big_Int b(k, 0);
-	for (int i = 0; i < k; ++i)
+	for (size_t i = 0; i < k; ++i)
 		b.number[i] = first.number[i];
 
 	Big_Int a(first.SIZE - k, 0);
-	for (int i = 0; i < a.SIZE; ++i)
+	for (size_t i = 0; i < a.SIZE; ++i)
 		a.number[i] = first.number[i + k];
 
 	Big_Int d(k, 0);
-	for (int i = 0; i < k; ++i)
+	for (size_t i = 0; i < k; ++i)
 		d.number[i] = second.number[i];
 
 	Big_Int c(second.SIZE - k, 0);
-	for (int i = 0; i < c.SIZE; ++i)
+	for (size_t i = 0; i < c.SIZE; ++i)
 		c.number[i] = second.number[i + k];
 
 	Big_Int a_plus_b = a + b;
@@ -345,8 +345,8 @@ Big_Int Big_Int::operator/(const Big_Int& other)
 void Big_Int::_squeeze()
 {
 	if ((*this) == Big_Int("0")) return;
-	int counter = 0;
-	for (int i = this->SIZE - 1; i != -1; --i)
+	size_t counter = 0;
+	for (size_t i = this->SIZE - 1; i != -1; --i)
 	{
 		if (this->number[i] == 0) counter++;
 		else break;
@@ -358,11 +358,11 @@ void Big_Int::_squeeze()
 		return;
 	};
 	unsigned long long* tmp = new unsigned long long[this->SIZE - counter];
-	for (int i = 0; i < this->SIZE - counter; ++i) tmp[i] = number[i];
+	for (size_t i = 0; i < this->SIZE - counter; ++i) tmp[i] = number[i];
 	delete[] this->number;
 	SIZE -= counter;
 	this->number = new unsigned long long[SIZE];
-	for (int i = 0; i < this->SIZE; ++i) number[i] = tmp[i];
+	for (size_t i = 0; i < this->SIZE; ++i) number[i] = tmp[i];
 	delete[] tmp;
 }
 
@@ -370,12 +370,12 @@ void Big_Int::_to_equal_size(Big_Int& first, Big_Int& other)
 {
 	if (first.SIZE == other.SIZE) return;
 	Big_Int tmp(max(first.SIZE, other.SIZE), 0);
-	if (first.SIZE < other.SIZE) for (int i = 0; i < first.SIZE; ++i)
+	if (first.SIZE < other.SIZE) for (size_t i = 0; i < first.SIZE; ++i)
 	{
 		tmp.number[i] = first.number[i];
 		first = tmp;
 	}
-	else for (int i = 0; i < other.SIZE; ++i)
+	else for (size_t i = 0; i < other.SIZE; ++i)
 	{
 		tmp.number[i] = other.number[i];
 		other = tmp;
@@ -386,12 +386,12 @@ void Big_Int::_increaseByModulo()
 {
 
 	unsigned long long* tmp = new unsigned long long[this->SIZE + 1];
-	for (int i = this->SIZE - 1; i != -1; --i)
+	for (size_t i = this->SIZE - 1; i != -1; --i)
 		tmp[i + 1] = this->number[i];
 	delete[] this->number;
 	tmp[0] = 0;
 	this->number = new unsigned long long[++SIZE];
-	for (int i = 0; i <this->SIZE; ++i)
+	for (size_t i = 0; i <this->SIZE; ++i)
 		this->number[i] = tmp[i];
 	delete[] tmp;
 }
@@ -405,7 +405,7 @@ bool Big_Int::operator<(const Big_Int& other) const
 		else if (other.SIZE < this->SIZE) return true;
 		else
 		{
-			for (int i = SIZE - 1; i != -1; --i)
+			for (size_t i = SIZE - 1; i != -1; --i)
 			{
 				if (this->number[i] < other.number[i]) return false;
 				else if (other.number[i] < this->number[i]) return true;
@@ -421,7 +421,7 @@ bool Big_Int::operator<(const Big_Int& other) const
 		else if (other.SIZE < this->SIZE) return false;
 		else 
 		{
-			for (int i = SIZE - 1; i != -1; --i)
+			for (size_t i = SIZE - 1; i != -1; --i)
 			{
 				if (this->number[i] < other.number[i]) return true;
 				else if (other.number[i] < this->number[i]) return false;
@@ -435,7 +435,7 @@ bool Big_Int::operator==(const Big_Int& other) const
 {
 	if (this->SIZE == other.SIZE && this->negative == other.negative)
 	{
-		for (int i = 0; i < SIZE; ++i)
+		for (size_t i = 0; i < SIZE; ++i)
 		{
 			if (this->number[i] != other.number[i]) return false;
 		}
